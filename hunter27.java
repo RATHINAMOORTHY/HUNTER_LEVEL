87c7a3136bb680bf7870c0167f28a300cfd5d7bc
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-10 11:11:44 +0530

    Create hunter27.java

---
+++ hunter27.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class hunter27
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("ENTER THE STRING");
+		String s=in.nextLine();
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		for(int i=0;i<s.length();i++)
+		{
+			for(int j=i;j<s.length();j++)
+			{
+				String str=s.substring(i,j+1);
+			    StringBuffer sb=new StringBuffer(str);
+				if(sb.equals(sb.reverse()))
+				{
+					al.add(str.length());
+				}
+			}
+		}
+		Collections.sort(al);
+		System.out.println(al.get(al.size()-1));
+	}
+}
+					
