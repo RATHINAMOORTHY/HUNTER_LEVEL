25c576763acb45842f8018ad38f425b53836c925
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-11 22:15:52 +0530

    Create hunter37.java

--- hunter37.java
+++ hunter37.java
@@ -8,11 +8,12 @@ public class hunter37 {
 		String s=in.nextLine();
 		for(int i=0;i<s.length();i++)
 		{
+			int e=i;
            String s1=(s.substring(0, i)+s.substring(i+1,s.length()));
 		   StringBuffer sb=new StringBuffer(s1);
 		   if(s1.equals(new String(sb.reverse())))
 		   {
-			   System.out.println(s1+" IS THE PALINDROME");
+			   System.out.println(s1+" IS THE PALINDROME BY REMOVING CHARACTER '"+s.charAt(e)+"'");
 		   }
 	
 		}
