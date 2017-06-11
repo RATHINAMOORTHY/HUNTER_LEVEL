4e84865b310e1c4df8521f35f3e07e7155fcda0b
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-11 22:09:26 +0530

    Create hunter37.java

---
+++ hunter37.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class hunter37 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("ENTER THE VALUE");
+		String s=in.nextLine();
+		for(int i=0;i<s.length();i++)
+		{
+           String s1=(s.substring(0, i)+s.substring(i+1,s.length()));
+		   StringBuffer sb=new StringBuffer(s1);
+		   if(s1.equals(new String(sb.reverse())))
+		   {
+			   System.out.println(s1+" IS THE PALINDROME");
+		   }
+	
+		}
+
+	}
+
+}
