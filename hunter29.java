1c129956a28d3f08e8b16861cb5f4189255f2e75
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-11 14:54:50 +0530

    Create hunter29.java

---
+++ hunter29.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class hunter29 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		System.out.println("ENTER THE SIZE OF ARRAY");
+		int n=in.nextInt();
+		int[] a=new int[n];
+		System.out.println("ENTER THE ELEMENTS FOR ARRAY");
+		for(int i=0;i<n;i++)
+		{
+			a[i]=in.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			int sum=0;
+			for(int j=i;j<n;j++)
+			{
+				sum=sum+a[i];
+				al.add(sum);
+			}
+		}
+		Collections.sort(al);
+		int s=(int)al.get(al.size()-1);
+		for(int i=0;i<n;i++)
+		{
+			int sum=0;
+			for(int j=i;j<n;j++)
+			{
+				sum=sum+a[i];
+				if(sum==s)
+				{
+					for(int k=i;k<n;k++)
+					{
+						System.out.print(a[k]+"+");
+					}
+					System.out.print("= "+sum);
+				}
+			}
+		}
+
+	}
+
+}
