1fbcd8d78004346dc8fdb5db8f71b912791ac446
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-12 06:35:25 +0530

    Create hunter13.java

---
+++ hunter13.java
@@ -0,0 +1,37 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class hunter13 {
+
+	public static void main(String[] args) {
+		LinkedList<Character> ll=new LinkedList<Character>();
+		String s="";
+		Scanner in=new Scanner(System.in);
+		System.out.println("ENTER THE SIZE OF LinkedList");
+		int n=in.nextInt();
+		char[] a=new char[n];
+		System.out.println("ENTER THE ELEMENTS FOR LinkedList");
+		for(int i=0;i<n;i++)
+		{
+			a[i]=in.next().charAt(0);
+			ll.add(a[i]);
+		}
+		for(char c:ll)
+		{
+			s=s.concat(String.valueOf(c));
+		}
+		StringBuffer sb=new StringBuffer(s);
+		if(s.equals(new String(sb.reverse())))
+		{
+			System.out.println("PALINDROME");
+		}
+		else
+		{
+			System.out.println("NOT A PALINDROME");
+		}
+		
+
+	}
+
+}
